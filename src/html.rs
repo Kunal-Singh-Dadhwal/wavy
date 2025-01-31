f9078bbebf56e3d3cbaa636952297d9fb640b9ed
Kunal Singh Dadhwal <kunalsinghdadhwal@gmail.com>
2025-01-31 19:05:32 +0530

    Html aur dom done

--- src/html.rs
+++ src/html.rs
@@ -40,4 +40,93 @@ impl Parser {
             self.parse_text()
         }
     }
+
+    fn parse_element(&mut self) -> dom::Node {
+        self.expect("<");
+        let tag_name = self.parse_name();
+        let attr = self.parse_attributes();
+        self.expect(">");
+
+        let children = self.parse_nodes();
+
+        self.expect("</");
+        self.expect(&tag_name);
+        self.expect(">");
+
+        dom::element(tag_name, attrs, children)
+    }
+
+    fn parse_name(&mut self) -> String {
+        self.consume_while(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9'))
+    }
+
+    fn parse_attributes(&mut self) -> dom::AttrMap {
+        let mut attributes = HashMap::new();
+        loop {
+            self.consume_whitespace();
+            if self.next_char() == ">" {
+                break;
+            }
+            let (name, value) = self.parse_attr();
+            attributes.insert(name, value);
+        }
+    }
+
+    fn parse_attr(&mut self) -> (String, String) {
+        let name = self.parse_name();
+        self.expect("=");
+        let value = self.parse_attr_value();
+        (name, value)
+    }
+
+    fn parse_attr_value(&mut self) -> String {
+        let open_quote = self.consume_char();
+        assert!(open_quote == "" || open_quote == "\'");
+        let value = self.consume_while(|c| c != open_quote);
+        let close_quote = self.consume_char();
+        assert_eq!(open_quote, close_quote);
+        value
+    }
+
+    fn parse_text(&mut self) -> dom::Node {
+        dom::text(self.consume_while(|c| c != "<"))
+    }
+
+    fn consume_whitespace(&mut self) {
+        self.consume_while(char::is_whitespace);
+    }
+
+    fn consume_while(&mut self, test: impl Fn(char) -> bool) -> String {
+        let mut result = String::new();
+        while !self.eof() && test(self.next_char()) {
+            result.push(self.consume_char());
+        }
+        result
+    }
+
+    fn consume_char(&mut self) -> char {
+        let c = self.next_char();
+        self.pos += c.len_utf8();
+        c
+    }
+
+    fn next_char(&self) -> char {
+        self.input[self.pos..].chars().next().unwrap()
+    }
+
+    fn starts_with(&self, s: &str) -> bool {
+        self.input[self.pos..].starts_with(s)
+    }
+
+    fn expect(&mut self, s: &str) {
+        if self.starts_with(s) {
+            self.pos += s.len();
+        } else {
+            panic!("Expected {:?} at byte {} but not found", s, self.pos);
+        }
+    }
+
+    fn eof(&self) -> bool {
+        self.pos >= self.input.len()
+    }
 }
